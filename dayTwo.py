d2e964acde62ca139abdaec34196ae986f98d916
Aolin Yang <yangal321@gmail.com>
2020-08-18 16:56:32 -0700

    task modification

--- dayTwo.py
+++ dayTwo.py
@@ -1,11 +1,11 @@
 ##TODO learning session
 1. 了解source tree以及branch的概念和特性，什么是master branch?
 2. 了解commit 和 push指令以及用法
-
+3. 什么是git checkout？
 
 
 ##TASKS
-1. clone master branch to local
-2. 吧昨天的summary提交到master branch里
-3. bonus: 新建一个自己的branch
+1. clone pyrepo master branch to local
+2. 吧Day1写的summary提交到自己创建的repo里
+3. 尝试对Day1 clone下来的repo使用checkout（tip： check what branch they have before you do checkout）
 
