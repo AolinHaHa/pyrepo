fbfe3843d6cf962287e42204789e964612eb96b0
Aolin Yang <yangal321@gmail.com>
2020-08-18 16:42:55 -0700

    adding context for day2

---
+++ dayTwo.py
@@ -0,0 +1,11 @@
+##TODO learning session
+1. 了解source tree以及branch的概念和特性，什么是master branch?
+2. 了解commit 和 push指令以及用法
+
+
+
+##TASKS
+1. clone master branch to local
+2. 吧昨天的summary提交到master branch里
+3. bonus: 新建一个自己的branch
+
