b91578dd15ec9346cebeab87de698ecdf8a9c475
Aolin Yang <yangal321@gmail.com>
2020-08-18 15:47:05 -0700

    adding first commit

--- myFirstTry.py
+++ myFirstTry.py
@@ -1,1 +1,13 @@
 ##first git practice
+
+##TODO learning session
+1. 什么是version control？ 它能解决什么问题？
+2. github是什么？
+3. 在github里，什么是repository？
+4. git clone的用法
+
+##TASKS
+1. 安装git/git desktop
+2. 创建自己的repository
+3. 用git clone一个你感兴趣的开源项目
+
