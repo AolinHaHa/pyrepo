3247a765652109b387aafdfffa8133186b1c03aa
Aolin Yang <yangal321@gmail.com>
2020-08-19 14:29:38 -0700

    day3 tasks

---
+++ day3.py
@@ -0,0 +1,11 @@
+##TODO learning session
+1. 什么是pr（pull request）？
+2. 了解add 以及 reset的实例和用法.
+3. 了解status 和 log的用法.
+
+
+##TASKS
+1. create a pr to include all your tasks and targeting to my master branch, save screenshot of your implementation.
+2. please separate commits in your pr (at least more than one commit)
+3. please add screenshot in your pr description/comments
+
