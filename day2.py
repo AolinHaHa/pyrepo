54c634e4021b38c3ffe6ea6838535c939f476516
Aolin Yang <yangal321@gmail.com>
2020-08-19 14:40:55 -0700

    day3 enhancement

--- day2.py
+++ day2.py
@@ -1,12 +1,12 @@
 ##TODO learning session
-1. 了解source tree以及branch的概念和特性，什么是master branch?
-2. 了解init, commit 和 push指令以及用法
-3. 什么是git checkout？
+##1. 了解source tree以及branch的概念和特性，什么是master branch?
+##2. 了解init, commit 和 push指令以及用法
+##3. 什么是git checkout？
 
 
 ##TASKS
-1. clone pyrepo master branch to local
-2. 吧Day1写的summary提交到自己创建的repo里
-3. 尝试对Day1 clone下来的repo使用checkout（tip： check what branch they have before you do checkout）
+##1. clone pyrepo master branch to local
+##2. 吧Day1写的summary提交到自己创建的repo里
+##3. 尝试对Day1 clone下来的repo使用checkout（tip： check what branch they have before you do checkout）
 
 
--- day3.py
+++ day3.py
@@ -1,11 +1,12 @@
 ##TODO learning session
-1. 什么是pr（pull request）？
-2. 了解add 以及 reset的实例和用法.
-3. 了解status 和 log的用法.
+##1. 什么是pull？ pull和clone的区别？
+##2. 了解add 以及 reset的实例和用法.
+##3. 了解status 和 log的用法.
+##4. 什么是pr(pull request)？
 
 
 ##TASKS
-1. create a pr to include all your tasks and targeting to my master branch, save screenshot of your implementation.
-2. please separate commits in your pr (at least more than one commit)
-3. please add screenshot in your pr description/comments
+##1. create a pr to include all your tasks and targeting to my master branch, save screenshot of your implementation.
+##2. please separate commits in your pr (at least more than one commit)
+##3. please add screenshot in your pr description/comments, and a brief intro of how you implemented it.
 
