3cf97f4d387d75d6c0eadb82bf166e3cbe87601b
Aolin Yang <yangal321@gmail.com>
2020-09-02 19:30:25 -0700

    day4 enhancement

--- day4.py
+++ day4.py
@@ -1,2 +1,11 @@
-#task
-git rebase
+##TODO learning session
+##1. 什么是rebase，rebase的优缺点(optional)
+##2. 先做task1，然后了解标准Software Development Life Cycle
+##3. 了解Relational Database, Relational Model 以及MySQL数据类型
+
+
+
+##TASKS
+##1. what is your software development life cycle looks like?
+##2. 你需要为Apple做一个员工管理系统，在创建数据库时你会注重哪些数据的存储? 请尝试画出relational model图
+
